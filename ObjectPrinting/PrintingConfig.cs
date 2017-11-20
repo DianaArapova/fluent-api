37388a2bb3391bd633cc73a201d144e1f8dbdbed
DianaArapova <diiana_arapova@mail.ru>
2017-11-20 21:07:10 +0500

    classwork

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using NUnit.Framework;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -37,5 +42,58 @@ namespace ObjectPrinting
             }
             return sb.ToString();
         }
+
+	    public PrintingConfig<TOwner> ExludeType<TProp>()
+	    {
+		    return this;
+	    }
+
+	    public PropertyPrintingConfing<TOwner, TPropType> Printing<TPropType>()
+	    {
+		    return new PropertyPrintingConfing<TOwner,TPropType>(this);
+	    }
+
+	    public void ExcludingProperty(Func<object, bool> func)
+	    {
+		    return;
+	    }
+
+	    public PropertyPrintingConfing<TOwner, TPropType> Printing<TPropType>
+			(Expression<Func<TOwner, TPropType>> selector)
+	    {
+			return new PropertyPrintingConfing<TOwner, TPropType>(this);
+	    }
     }
+
+	public static class PropertyPrintingConfingExtention
+	{
+		public static PrintingConfig<TOwner> Using<TOwner>(
+			this PropertyPrintingConfing<TOwner, int> a,
+			CultureInfo cultureInfo)
+		{
+			return ((IPropertyPrintingConfig<TOwner, int>) a).PrintingConfig;
+		}
+	}
+
+	public class PropertyPrintingConfing<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
+	{
+		private readonly PrintingConfig<TOwner> printingConfig;
+		public PropertyPrintingConfing(PrintingConfig<TOwner> printingConfig)
+		{
+			this.printingConfig = printingConfig;
+		}
+
+		public PrintingConfig<TOwner> Using(Func<TPropType, string> seializeFunc)
+		{
+			return printingConfig;
+		}
+
+		PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.
+			PrintingConfig => printingConfig;
+	}
+
+	public interface IPropertyPrintingConfig<TOwner, TProperty>
+	{
+		PrintingConfig<TOwner> PrintingConfig { get; }
+	}
 }
